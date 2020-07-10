37202eac317c01906d921b757e84456332a5405f
Iam  Thesenate <apc.freitas@sapo.pt>
2020-07-10 11:20:57 +0100

    done for now; could do pawn promotion, waiting for partner/disconnect screen, allowed moves, last opponent move highlight, captured pieces, account; records...

--- server.py
+++ server.py
@@ -15,8 +15,8 @@ s.listen(2)
 
 print("Waiting for a connection, Server Started")
 
-players={0:'whites', 1:'blacks'}
-last_move={0:'0', 1:'0'}
+players={}
+last_move={}
 
 def client_thread(conn, player):
     conn.send(str.encode(players[player]))
@@ -24,10 +24,12 @@ def client_thread(conn, player):
     
     while True:
         try:
-            if player==0:
-                reply=last_move[1]
-            if player==1:
-                reply=last_move[0]
+            if player%2==0:
+                reply=last_move[player+1]
+                print('player1: '+str(reply))
+            elif player%2==1:
+                reply=last_move[player-1]
+                print('player2: '+str(reply))
 
             conn.sendall(str.encode(reply))
             
@@ -39,19 +41,54 @@ def client_thread(conn, player):
                 last_move[player]='0'
                 
         except Exception as e:
-            break
+            pass
+    
+    try:
+        if player%2==0:
+            del(players[player])
+            del(last_move[player])
+            del(players[player+1])
+            del(last_move[player+1])
+        elif player%2==1:
+            del(players[player])
+            del(last_move[player])
+            del(players[player-1])
+            del(last_move[player-1])
+    except KeyError:
+        pass
+
+    print(players)
+    print(last_move)
     print("Lost connection")
+
     conn.close()
 
 current_player=0
 while True:
-    if current_player>1:
-        current_player=0
-        players={0:'whites', 1:'blacks'}
-        last_move={0:'0', 1:'0'}
+    
     conn, addr=s.accept()
-    print("Connected to:", addr)
+
+    if len(players)==0:
+        current_player=0
+    else:
+        for _ in range(len(players)):
+            for k in players.keys():
+                if _ not in players:
+                    if _+1 not in players:
+                        current_player=_
+                else:
+                    if _+1 not in players:
+                        current_player=_+1
+
+    if current_player%2==0:
+        players[current_player]='whites'
+        last_move[current_player]='0'
+    else:
+        players[current_player]='blacks'
+        last_move[current_player]='0'
+
+
+    print("Connected to:", addr, "Current Player: ", current_player, 'players:', players, 'last move: ', last_move)
 
     new_client=threading.Thread(target=client_thread, args=(conn,current_player))
     new_client.start()
-    current_player+=1
